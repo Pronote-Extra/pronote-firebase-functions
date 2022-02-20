62c7e52e397a1850c873a32631e2b8974e76d81e
science-math-guy <adumont@mailbox.org>
2022-02-21 00:39:27 +0100

    First function 🚀

--- functions/index.js
+++ functions/index.js
@@ -3,24 +3,46 @@ const admin = require('firebase-admin');
 const axios = require('axios');
 const qs = require('qs');
 const dotenv = require('dotenv');
-const cors = require('cors')({ origin: true });
+const cors = require('cors')({
+    origin: true
+});
 const moment = require('moment-timezone');
+const nodemailer = require("nodemailer");
+const { localeData } = require("moment-timezone");
 
-exports.testTwitterAPI = functions.https.onRequest(async (req, res) => {
-    let url = `https://twitter-trends-analyser.herokuapp.com/api/twittertrends`;
+exports.pronoteAPI = functions.https.onRequest(async (req, res) => {
+    let url = process.env.PRONOTE_API_LINK;
     let data = [];
-    const db = getDatabase();
     await axios.post(url, {}, {
         auth: {
             username: process.env.USERNAME,
-            password: process.env.PASSWORD
+            pass: process.env.PASSWORD
         }
     }).then((res) => {
         data = res.data
-        const ref = db.ref('data/twittertrends/JimCramer').update({
-            stocksToBuy: data,
-            date: moment().tz("Europe/Paris").format()
-        });
     });
+    // let transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: process.env.EMAIl,
+    //         pass: process.env.EMAIl_PASSWORD
+    //     }
+    // });
+      
+    // let mailOptions = {
+    //     from: process.env.EMAIl,
+    //     to: process.env.EMAIl,
+    //     subject: 'Sending Email using Node.js',
+    //     text: 'That was easy!'
+    // };
+      
+    // transporter.sendMail(mailOptions, function(error, info){
+    //     if (error) {
+    //         console.log(error);
+    //     } else {
+    //         console.log('Email sent: ' + info.response);
+    //     }
+    // });
+
     res.send(data);
 });
